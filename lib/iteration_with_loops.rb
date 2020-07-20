c7422dbebd897fb09f67bc4ccec69b1b923a9b75
Melinda Diaz <melindadiaz@me.com>
2020-07-20 17:19:38 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,6 +1,18 @@
 def find_min_in_nested_arrays(src)
+ smallest_numbers_array = []
+ count = 0 
  
+ while count < src.length do
+   inner_count = 0 
+   
+   while inner_count < src[count].length do
+     new_array = src[count][inner_count]
+     
+     
+     
+   end 
+  end
   
-
+smallest_numbers_array
 
 end
